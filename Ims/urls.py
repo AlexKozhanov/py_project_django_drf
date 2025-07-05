dad4db2c7ae25e66a7c2f56d7e86d1f7dede2be6
Admin <alexander_kozhanov@mail.ru>
2025-07-05 16:48:16 +0300

    working on django30.1

--- Ims/urls.py
+++ Ims/urls.py
@@ -1,6 +1,12 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from Ims.views import CourseViewSet
+from Ims.views import   CourseViewSet,\
+                        LessonListAPIView,\
+                        LessonCreateAPIView,\
+                        LessonRetrieveAPIView,\
+                        LessonUpdateAPIView,\
+                        LessonDestroyAPIView
 from Ims.apps import ImsConfig
 
 app_name = ImsConfig.name
@@ -8,6 +14,12 @@ app_name = ImsConfig.name
 router = SimpleRouter()
 router.register('', CourseViewSet)
 
-urlpatterns = []
+urlpatterns = [
+    path("lesson/", LessonListAPIView.as_view(), name='lesson_list'),
+    path("lesson/create/", LessonCreateAPIView.as_view(), name='lesson_create'),
+    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name='lesson_retrieve'),
+    path("lesson/<int:pk>/update/", LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path("lesson/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name='lesson_delete'),
+]
 
 urlpatterns += router.urls
